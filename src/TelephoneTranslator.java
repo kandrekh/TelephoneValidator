5846b990f20083412e47e0d09972724829cfb5a4
K-Andre Harris <123778827+kdreh@users.noreply.github.com>
2024-05-13 09:40:08 -0400

    Code Refactor
    Rename functions and fixed spelling
    Add descriptions to @phoneNumber tags
    removed redundant type casting for matcher
    refactor swich statement

--- src/TelephoneTranslator.java
+++ src/TelephoneTranslator.java
@@ -1,12 +1,11 @@
 import javax.swing.*;
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Program: Alphnumeric telelphone number translator
+ * Program: Alphanumeric telephone number translator
  * Author: K-Andre Harris
- * Date Created: April 10, 2024
+ * Date Created: April 10, 2024,
  * Date Modified: May 10, 2024
 
  * Course: CIT 111-Z02 Intro to Prog: Java
@@ -32,14 +31,14 @@ public class TelephoneTranslator {
     public static void validation(){
         String phoneNumber = getNumber();
         if(phoneNumberPatternValidation(phoneNumber)){
-            numuericPhoneNumber(phoneNumber);
+            numericPhoneNumber(phoneNumber);
         }else if (!phoneNumberPatternValidation(phoneNumber)){
             JOptionPane.showMessageDialog(null,"Invalid");
         }
     }
     /**
      * Validate the user input to match xxx-xxx-xxxx
-     * @param phoneNumber
+     * @param phoneNumber alphanumeric phone number inputted by user
      * @return boolean if phone number pattern matches
      */
     public static boolean phoneNumberPatternValidation(String phoneNumber ){
@@ -49,61 +48,28 @@ public class TelephoneTranslator {
         Pattern pattern = Pattern.compile(numberPattern);
         Matcher matcher = pattern.matcher(phoneNumber);
 
-        return ((Matcher) matcher).matches();
+        return matcher.matches();
     }
     /**
      * a Gets phoneNumber and displays the numeric values of the inputted number
-     * @param phoneNumber
+     * @param phoneNumber alphanumeric phone number inputted by user to be converted
      */
-    public  static void numuericPhoneNumber(String phoneNumber) {
+    public  static void numericPhoneNumber(String phoneNumber) {
         long phoneNumberFinal =0;
         for (int i = 0; i < phoneNumber.length(); i++){
             char ch = phoneNumber.charAt(i);
             if (Character.isLetter(ch)) {
-                switch (ch) {
-                    case 'A':
-                    case 'B':
-                    case 'C':
-                        phoneNumberFinal = phoneNumberFinal * 10 + 2;
-                        break;
-                    case 'D':
-                    case 'E':
-                    case 'F':
-                        phoneNumberFinal = phoneNumberFinal * 10 + 3;
-                        break;
-                    case 'G':
-                    case 'H':
-                    case 'I':
-                        phoneNumberFinal = phoneNumberFinal * 10 + 4;
-                        break;
-                    case 'J':
-                    case 'K':
-                    case 'L':
-                        phoneNumberFinal = phoneNumberFinal * 10 + 5;
-                        break;
-                    case 'M':
-                    case 'N':
-                    case 'O':
-                        phoneNumberFinal = phoneNumberFinal * 10 + 6;
-                        break;
-                    case 'P':
-                    case 'Q':
-                    case 'R':
-                    case 'S':
-                        phoneNumberFinal = phoneNumberFinal * 10 + 7;
-                        break;
-                    case 'T':
-                    case 'U':
-                    case 'V':
-                        phoneNumberFinal = phoneNumberFinal * 10 + 8;
-                        break;
-                    case 'W':
-                    case 'X':
-                    case 'Y':
-                    case 'Z':
-                        phoneNumberFinal = phoneNumberFinal * 10 + 9;
-                        break;
-                }
+                phoneNumberFinal = switch (ch) {
+                    case 'A', 'B', 'C' -> phoneNumberFinal * 10 + 2;
+                    case 'D', 'E', 'F' -> phoneNumberFinal * 10 + 3;
+                    case 'G', 'H', 'I' -> phoneNumberFinal * 10 + 4;
+                    case 'J', 'K', 'L' -> phoneNumberFinal * 10 + 5;
+                    case 'M', 'N', 'O' -> phoneNumberFinal * 10 + 6;
+                    case 'P', 'Q', 'R', 'S' -> phoneNumberFinal * 10 + 7;
+                    case 'T', 'U', 'V' -> phoneNumberFinal * 10 + 8;
+                    case 'W', 'X', 'Y', 'Z' -> phoneNumberFinal * 10 + 9;
+                    default -> phoneNumberFinal;
+                };
             }
             else if (Character.isDigit(ch)) {
                 phoneNumberFinal = phoneNumberFinal * 10 + Character.getNumericValue(ch);
